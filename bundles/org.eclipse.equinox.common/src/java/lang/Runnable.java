651607d45a29fe5e74e3dc26ae0a6574bd97e653
Boris Bokowski <bbokowski>
2007-12-11 14:52:09 +0000

    Added comments

--- bundles/org.eclipse.equinox.common/src/java/lang/Runnable.java
+++ bundles/org.eclipse.equinox.common/src/java/lang/Runnable.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2005, 2007 IBM Corporation and others.
+ * Copyright (c) 2007 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,6 +10,10 @@
  *******************************************************************************/
 package java.lang;
 
+/**
+ * This interface should really be part of the GWT class libraries.
+ *
+ */
 public interface Runnable {
 	public void run();
 }
