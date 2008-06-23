bc08ea3cceb116e326751edee549ab6cb7bed270
khorne <khorne>
2008-06-23 14:22:22 +0000

    Removed SWT exception, faster opening of shell

--- bundles/org.eclipse.ui.tools/src/org/eclipse/ui/tools/Activator.java
+++ bundles/org.eclipse.ui.tools/src/org/eclipse/ui/tools/Activator.java
@@ -39,6 +39,8 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 
 	private Listener filter;
 
+	private Shell hiddenShell;
+
 	/**
 	 * The constructor
 	 */
@@ -59,10 +61,11 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 
 		filter = new Listener() {
 
+			// the magic sequence most children of the 80s can recite from
+			// memory
 			final int[] correctChars = { SWT.ARROW_UP, SWT.ARROW_UP,
 					SWT.ARROW_DOWN, SWT.ARROW_DOWN, SWT.ARROW_LEFT,
-					SWT.ARROW_RIGHT, SWT.ARROW_LEFT, SWT.ARROW_RIGHT,
-					(int) 'b', (int) 'a' };
+					SWT.ARROW_RIGHT, SWT.ARROW_LEFT, SWT.ARROW_RIGHT, 'b', 'a' };
 			int currentCorrectCount = 0;
 
 			public void handleEvent(Event event) {
@@ -70,79 +73,96 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 					currentCorrectCount++;
 					if (currentCorrectCount == correctChars.length) {
 						currentCorrectCount = 0;
-						doCrazyStuff(event.display);
+						openHiddenShell(event.display);
 					}
 					return;
 				}
 				currentCorrectCount = 0;
 			}
 
-			private void doCrazyStuff(final Display display) {
+			private void openHiddenShell(final Display display) {
 				display.asyncExec(new Runnable() {
 
 					public void run() {
-						final Image image = imageDescriptorFromPlugin(
-								"org.eclipse.ui.tools", "icons/o_rly.png").createImage(); //$NON-NLS-1$ //$NON-NLS-2$
 						Shell parentShell = display.getActiveShell();
-						final Shell shell = new Shell(parentShell, SWT.NO_TRIM);
-						final Region region = new Region();
-						final ImageData imageData = image.getImageData();
-						if (imageData.alphaData != null) {
-							Rectangle pixel = new Rectangle(0, 0, 1, 1);
-							for (int y = 0; y < imageData.height; y++) {
-								for (int x = 0; x < imageData.width; x++) {
-									if (imageData.getAlpha(x, y) == 255) {
-										pixel.x = imageData.x + x;
-										pixel.y = imageData.y + y;
-										region.add(pixel);
+
+						// we're going to cache this shell because it's slow to
+						// calculate the mask. If you're concerned with the
+						// memory useage perhaps you shouldn't open it. ;)
+						if (hiddenShell == null) {
+							final Image image = imageDescriptorFromPlugin(
+									"org.eclipse.ui.tools", "icons/o_rly.png").createImage(); //$NON-NLS-1$ //$NON-NLS-2$
+							// open as a top level shell - we'll move it above
+							// shells as need be
+							hiddenShell = new Shell(display, SWT.NO_TRIM);
+							final Region region = new Region();
+							final ImageData imageData = image.getImageData();
+							// the following mask code is from an SWT snippet.
+							// It's very useful when drawing owls.
+							if (imageData.alphaData != null) {
+								Rectangle pixel = new Rectangle(0, 0, 1, 1);
+								for (int y = 0; y < imageData.height; y++) {
+									for (int x = 0; x < imageData.width; x++) {
+										if (imageData.getAlpha(x, y) == 255) {
+											pixel.x = imageData.x + x;
+											pixel.y = imageData.y + y;
+											region.add(pixel);
+										}
+									}
+								}
+							} else {
+								ImageData mask = imageData
+										.getTransparencyMask();
+								Rectangle pixel = new Rectangle(0, 0, 1, 1);
+								for (int y = 0; y < mask.height; y++) {
+									for (int x = 0; x < mask.width; x++) {
+										if (mask.getPixel(x, y) != 0) {
+											pixel.x = imageData.x + x;
+											pixel.y = imageData.y + y;
+											region.add(pixel);
+										}
 									}
 								}
 							}
-						} else {
-							ImageData mask = imageData.getTransparencyMask();
-							Rectangle pixel = new Rectangle(0, 0, 1, 1);
-							for (int y = 0; y < mask.height; y++) {
-								for (int x = 0; x < mask.width; x++) {
-									if (mask.getPixel(x, y) != 0) {
-										pixel.x = imageData.x + x;
-										pixel.y = imageData.y + y;
-										region.add(pixel);
+							hiddenShell.setRegion(region);
+
+							Listener l = new Listener() {
+
+								public void handleEvent(Event e) {
+									if (e.type == SWT.KeyDown) {
+										// dont actually dispose the shell -
+										// just hide it
+										hiddenShell.setVisible(false);
+									}
+									if (e.type == SWT.Paint) {
+										e.gc.drawImage(image, imageData.x,
+												imageData.y);
+									}
+									if (e.type == SWT.Dispose) {
+										image.dispose();
+										region.dispose();
 									}
 								}
-							}
+							};
+							hiddenShell.addListener(SWT.KeyDown, l);
+							hiddenShell.addListener(SWT.Paint, l);
+							hiddenShell.addListener(SWT.Dispose, l);
+
+							hiddenShell.setSize(imageData.x + imageData.width,
+									imageData.y + imageData.height);
 						}
-						shell.setRegion(region);
 
-						Listener l = new Listener() {
-
-							public void handleEvent(Event e) {
-								if (e.type == SWT.KeyDown) {
-									shell.dispose();
-								}
-								if (e.type == SWT.Paint) {
-									e.gc.drawImage(image, imageData.x,
-											imageData.y);
-								}
-								if (e.type == SWT.Dispose) {
-									region.dispose();
-									image.dispose();
-								}
-							}
-						};
-						shell.addListener(SWT.KeyDown, l);
-						shell.addListener(SWT.Paint, l);
-						shell.addListener(SWT.Dispose, l);
-
-						shell.setSize(imageData.x + imageData.width,
-								imageData.y + imageData.height);
 						if (parentShell != null) {
-
-							shell.setLocation(parentShell.getLocation().x,
-									parentShell.getBounds().height
-											- shell.getSize().y
+							// move the shell relative to the parent
+							hiddenShell.setLocation(
+									parentShell.getLocation().x, parentShell
+											.getBounds().height
+											- hiddenShell.getSize().y
 											+ parentShell.getBounds().y);
+							// move it above the parent
+							hiddenShell.moveAbove(parentShell);
 						}
-						shell.open();
+						hiddenShell.open();
 
 					}
 				});
@@ -162,7 +182,11 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 		plugin = null;
 		this.context = null;
 		super.stop(context);
-		PlatformUI.getWorkbench().getDisplay().removeFilter(SWT.KeyUp, filter);
+		if (hiddenShell != null && !hiddenShell.isDisposed())
+			hiddenShell.dispose();
+		Display display = PlatformUI.getWorkbench().getDisplay();
+		if (!display.isDisposed())
+			display.removeFilter(SWT.KeyUp, filter);
 	}
 
 	/**
@@ -185,5 +209,4 @@ public class Activator extends AbstractUIPlugin implements IStartup {
 	 */
 	public void earlyStartup() {
 	}
-
 }
