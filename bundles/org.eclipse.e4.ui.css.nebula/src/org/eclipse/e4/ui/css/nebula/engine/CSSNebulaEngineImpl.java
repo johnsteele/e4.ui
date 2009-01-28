7f2b4f0b1cd27ab7041ec13775ae8c2e90e776f5
kevinm <kevinm>
2009-01-28 23:57:05 +0000

    Initial commit of bug 256844 - [CSS] TK-UI CSS engine for Nebula widget

---
+++ bundles/org.eclipse.e4.ui.css.nebula/src/org/eclipse/e4/ui/css/nebula/engine/CSSNebulaEngineImpl.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Angelo Zerr and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.e4.ui.css.nebula.engine;
+
+import org.eclipse.e4.ui.css.core.dom.properties.css2.ICSSPropertyBackgroundHandler;
+import org.eclipse.e4.ui.css.core.dom.properties.css2.ICSSPropertyTextHandler;
+import org.eclipse.e4.ui.css.nebula.dom.NebulaElementProvider;
+import org.eclipse.e4.ui.css.nebula.properties.css2.CSSPropertyBackgroundNebulaHandler;
+import org.eclipse.e4.ui.css.nebula.properties.css2.CSSPropertyTextNebulaHandler;
+import org.eclipse.e4.ui.css.swt.engine.CSSSWTEngineImpl;
+import org.eclipse.swt.widgets.Display;
+
+public class CSSNebulaEngineImpl extends CSSSWTEngineImpl {
+
+	public CSSNebulaEngineImpl(Display display) {
+		super(display);
+		this.initializeNebulaConfig();
+	}
+
+	public CSSNebulaEngineImpl(Display display, boolean lazyApplyingStyles) {
+		super(display, lazyApplyingStyles);
+		this.initializeNebulaConfig();
+	}
+
+	protected void initializeNebulaConfig() {
+		// Register Nebula Element Provider to retrieve
+		// w3c Element NebulaElement which wrap SWT/Nebula widget.
+		super.setElementProvider(NebulaElementProvider.INSTANCE);
+	}
+
+	protected void initializeCSSPropertyHandlers() {
+		// Register Nebula CSS Property Background Handler
+		super.registerCSSPropertyHandler(ICSSPropertyBackgroundHandler.class,
+				CSSPropertyBackgroundNebulaHandler.INSTANCE);
+		// Register Nebula CSS Property Text Handler
+		super.registerCSSPropertyHandler(ICSSPropertyTextHandler.class,
+				CSSPropertyTextNebulaHandler.INSTANCE);
+		super.initializeCSSPropertyHandlers();
+	}
+
+}
---
+++ bundles/org.eclipse.e4.ui.css.nebula/src/org/eclipse/e4/ui/css/nebula/gallery/ListItemRendererDelegate.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2008 Angelo Zerr and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Angelo Zerr <angelo.zerr@gmail.com> - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.e4.ui.css.nebula.gallery;
+
+import org.eclipse.nebula.widgets.gallery.ListItemRenderer;
+import org.eclipse.swt.graphics.Color;
+
+public class ListItemRendererDelegate implements IGalleryItemRenderer {
+
+	private ListItemRenderer itemRenderer;
+
+	public ListItemRendererDelegate(
+			ListItemRenderer itemRenderer) {
+		this.itemRenderer = itemRenderer;
+	}
+
+	public Color getBackgroundColor() {
+		return itemRenderer.getBackgroundColor();
+	}
+
+	public Color getForegroundColor() {
+		return itemRenderer.getForegroundColor();
+	}
+
+	public Color getSelectionBackgroundColor() {
+		return itemRenderer.getSelectionBackgroundColor();
+	}
+
+	public Color getSelectionForegroundColor() {
+		return itemRenderer.getSelectionForegroundColor();
+	}
+
+	public void setBackgroundColor(Color backgroundColor) {
+		itemRenderer.setBackgroundColor(backgroundColor);
+	}
+
+	public void setForegroundColor(Color foregroundColor) {
+		itemRenderer.setForegroundColor(foregroundColor);
+	}
+
+	public void setSelectionBackgroundColor(Color selectionBackgroundColor) {
+		itemRenderer.setSelectionBackgroundColor(selectionBackgroundColor);
+	}
+
+	public void setSelectionForegroundColor(Color selectionForegroundColor) {
+		itemRenderer.setSelectionForegroundColor(selectionForegroundColor);
+	}
+
+}
