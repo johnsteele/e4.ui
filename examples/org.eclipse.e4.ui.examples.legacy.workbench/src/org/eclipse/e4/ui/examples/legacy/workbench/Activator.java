6ce110607d43c3a997d5d403e239decad10f43ae
Eric Moffatt <emoffatt>
2009-02-13 20:05:38 +0000

    Fix missing NLS tag

--- examples/org.eclipse.e4.ui.examples.legacy.workbench/src/org/eclipse/e4/ui/examples/legacy/workbench/Activator.java
+++ examples/org.eclipse.e4.ui.examples.legacy.workbench/src/org/eclipse/e4/ui/examples/legacy/workbench/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.e4.ui.examples.legacy.workbench";
+	public static final String PLUGIN_ID = "org.eclipse.e4.ui.examples.legacy.workbench"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
