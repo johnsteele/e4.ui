c4715dd44c9e764c14c0bcf6cf736ba68a4833e8
Eric Moffatt <emoffatt>
2009-02-22 23:57:36 +0000

    Fix for Bug 265777 - [Compatibility] LegacyWBWImpl#getInput() returns null

---
+++ examples/org.eclipse.e4.ui.examples.legacy.workbench/src/org/eclipse/e4/ui/examples/legacy/workbench/WorkspaceValue.java
@@ -0,0 +1,14 @@
+package org.eclipse.e4.ui.examples.legacy.workbench;
+
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.e4.core.services.context.IComputedValue;
+import org.eclipse.e4.core.services.context.IEclipseContext;
+
+public class WorkspaceValue implements IComputedValue {
+
+	public Object compute(IEclipseContext context, Object[] arguments) {
+		ResourcesPlugin.getPlugin().getPluginPreferences().setValue(ResourcesPlugin.PREF_AUTO_REFRESH, true);
+		return ResourcesPlugin.getWorkspace();
+	}
+
+}
