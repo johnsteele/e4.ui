8ed8736e1237db4224913ab5056b25aa3493186f
Paul Webster <pwebster>
2009-10-28 11:53:06 +0000

    Don't keep sending failed mail

--- bundles/org.eclipse.ui.releng/builder/runBuild.sh
+++ bundles/org.eclipse.ui.releng/builder/runBuild.sh
@@ -55,9 +55,9 @@ qfailed=$?
 
 if [ $qfailed = 1 ]; then
 rm -rf ${buildTopDir}/tests/test-eclipse
-#sendComplete
+echo sendComplete
 else
-#sendFailures
+echo sendFailures
 fi
 
 cp $optLocal/runBuild.log $buildDir
