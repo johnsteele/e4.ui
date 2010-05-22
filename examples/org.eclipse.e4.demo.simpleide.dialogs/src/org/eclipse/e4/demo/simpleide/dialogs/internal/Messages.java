b7b32cde3a795ca416fd1514b0eb6989faa6020d
Thomas Schindl <tschindl>
2010-05-22 16:36:39 +0000

    Bug 312938 -  [Demo] Add example of a Simple-IDE * adding new standard dialogs

---
+++ examples/org.eclipse.e4.demo.simpleide.dialogs/src/org/eclipse/e4/demo/simpleide/dialogs/internal/Messages.java
@@ -0,0 +1,8 @@
+package org.eclipse.e4.demo.simpleide.dialogs.internal;
+
+public class Messages {
+
+	public static String SelectionDialog_selectLabel;
+	public static String SelectionDialog_deselectLabel;
+
+}
