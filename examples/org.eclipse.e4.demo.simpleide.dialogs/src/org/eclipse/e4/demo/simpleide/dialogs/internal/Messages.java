ec7f3bd3e1c28bab4faa96c75b606a99c4993bb0
Thomas Schindl <tschindl>
2010-05-23 15:30:02 +0000

    Bug 312938 -  [Demo] Add example of a Simple-IDE * c&p some more dialog stuff from 3.x

--- examples/org.eclipse.e4.demo.simpleide.dialogs/src/org/eclipse/e4/demo/simpleide/dialogs/internal/Messages.java
+++ examples/org.eclipse.e4.demo.simpleide.dialogs/src/org/eclipse/e4/demo/simpleide/dialogs/internal/Messages.java
@@ -1,8 +1,25 @@
 package org.eclipse.e4.demo.simpleide.dialogs.internal;
 
-public class Messages {
+import org.eclipse.osgi.util.NLS;
 
+public class Messages {
+	
 	public static String SelectionDialog_selectLabel;
 	public static String SelectionDialog_deselectLabel;
+	
+	public static String GoHome_text;
+	public static String GoHome_toolTip;
+	public static String GoBack_toolTip;
+	public static String GoInto_text;
+	public static String GoInto_toolTip;
+	public static String GoBack_text;
+	
+	public static String ContainerGroup_message;
+	public static String ContainerGroup_selectFolder;
+	public static String ContainerSelectionDialog_title;
+	public static String ContainerSelectionDialog_message;
 
+	static {
+		NLS.initializeMessages(Messages.class.getName(), Messages.class);
+	}
 }
